586077b3c9d706e1165340c032a4a00e1d3ae20a
Ran.Jin, Dave <dave.ran.jin@kingland.com>
2020-08-20 10:54:18 +0800

    Update lunch learn code segment

--- src/app/training/rxjs-demo/higher-order-observables/case-study/continuous-form/continuous-form.component.ts
+++ src/app/training/rxjs-demo/higher-order-observables/case-study/continuous-form/continuous-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
-import { Observable, of, Subject, from } from 'rxjs';
-import { concatMap, takeUntil, map, delay } from 'rxjs/operators';
+import { Observable, of, Subject, from, merge } from 'rxjs';
+import { concatMap, takeUntil, map, delay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-continuous-form',
@@ -20,39 +20,43 @@ export class ContinuousFormComponent implements OnInit {
       name: new FormControl('', []),
       desc: new FormControl('', []),
       item: new FormControl('', []),
-      listCost: new FormControl('', [])
+      listCost: new FormControl('', []),
+      status: new FormControl('waiting', []),
+      check1: new FormControl(false, []),
+      check2: new FormControl(false, []),
+      check3: new FormControl(false, []),
+      check4: new FormControl(false, [])
     });
   }
 
   ngOnInit(): void {
     this.checkValueAction
       .pipe(
-        delay(5000),
-        map((e: any, index: number) => {
+        concatMap((e: any, index: number) => {
           this.queue[index].status = 'inProgress';
+          return this.checkValueTask(index, e);
         }),
-        delay(2000),
-        concatMap((e: any, index: number) => this.checkValueTask(index, e)),
-        map((e: any) => {
+        tap((e: any) => {
           this.queue[e.index][e.type] = true;
-          this.queue[e.index].status = 'complete';
         })
       )
-      .subscribe(e => console.log(e));
+      .subscribe(e => {
+        console.log(e);
+        if (
+          this.queue[e.index].check1 &&
+          this.queue[e.index].check2 &&
+          this.queue[e.index].check3 &&
+          this.queue[e.index].check4
+        ) {
+          this.queue[e.index].status = 'complete';
+        }
+      });
   }
 
   public onEmit(): void {
     const data = this.form.getRawValue();
-    const checkStatus = {
-      status: 'waiting',
-      check1: false,
-      check2: false,
-      check3: false,
-      check4: false
-    };
-    const hydrateData = Object.assign(data, checkStatus);
-    this.queue.push(hydrateData);
-    this.checkValueAction.next(hydrateData);
+    this.queue.push(data);
+    this.checkValueAction.next(data);
     // this.form.patchValue({
     //   name: '',
     //   desc: '',
@@ -62,11 +66,12 @@ export class ContinuousFormComponent implements OnInit {
   }
 
   private checkValueTask(index: number, data: any): Observable<any> {
-    return from([
-      { index: index, data: data, type: 'check1' },
-      { index: index, data: data, type: 'check2' },
-      { index: index, data: data, type: 'check3' },
-      { index: index, data: data, type: 'check4' }
-    ]);
+    console.log(index, data);
+    return merge(
+      of({ index: index, data: data, type: 'check1' }).pipe(delay(2000)),
+      of({ index: index, data: data, type: 'check2' }).pipe(delay(1500)),
+      of({ index: index, data: data, type: 'check3' }).pipe(delay(3000)),
+      of({ index: index, data: data, type: 'check4' }).pipe(delay(2500))
+    );
   }
 }
